5e4d70d2bfb4b71228c092d71f698120f5410391
Ali Raza <17140095@gift.edu.pk>
2023-04-19 12:49:04 +0500

    Update cart items

--- src/Components/CartItems.js
+++ src/Components/CartItems.js
@@ -1,5 +1,5 @@
-import { Text, Box, Center, Pressable, HStack } from "native-base";
-import SwipeListView from "react-native-swipe-list-view";
+import { Text, Box, Center, Pressable, HStack, Image, VStack, Button } from "native-base";
+import {SwipeListView} from "react-native-swipe-list-view";
 import Products from "../data/Products";
 import Colors from "../Colors/colors";
 import { FontAwesome } from "@expo/vector-icons";
@@ -7,7 +7,7 @@ import { FontAwesome } from "@expo/vector-icons";
 const Swiper = () => (
   <SwipeListView
     rightOpenValue={-50}
-    previewRowKey="0"
+    previewRowKey={"0"}
     previewOpenValue={-40}
     previewOpenDelay={3000}
     data={Products.slice(0, 2)}
@@ -17,18 +17,33 @@ const Swiper = () => (
   />
 );
 
-const renderItems = (data, rowMap) => (
+const renderItems = (data) => (
   <Pressable>
     <Box ml={6} mb={3}>
-      {/* <HStack
+      <HStack
         alignItems="center"
         bg={Colors.white}
         shadow={1}
         rounded={10}
         overflow="hidden"
       >
-        <Center w="25%" bg={Colors.primaryLight}></Center>
-      </HStack> */}
+        <Center w="25%" bg={Colors.primaryLight}>
+          <Image 
+            source={{uri: data.item.image}}
+            alt={data.item.name}
+            w="full"
+            h={24}
+            resizeMode="contain"
+          />
+        </Center>
+        <VStack p={2} w="60%" space={3}>
+          <Text bold isTruncated fontSize={14} color={Colors.black} >{data.item.name}</Text>
+          <Text color={Colors.grey} bold>${data.item.price}</Text>
+        </VStack>
+        <Button bg={Colors.primary} _pressed={{bg: Colors.primary}} _text={{color: Colors.white}}>
+          5
+        </Button>
+      </HStack>
     </Box>
   </Pressable>
 );
@@ -42,16 +57,20 @@ const hiddenItems = () => (
     justifyContent="center"
     bg={Colors.red}
   >
-    {/* <Center alignItems="center" space={2}>
+    <Center alignItems="center">
       <FontAwesome name="trash" size={24} color={Colors.white} />
-    </Center> */}
+    </Center>
   </Pressable>
 );
 
-export default function CartItems() {
+
+
+const CartItems = () => {
   return (
     <Box mr={6}>
       <Swiper />
     </Box>
   );
-}
+};
+
+export default CartItems;
